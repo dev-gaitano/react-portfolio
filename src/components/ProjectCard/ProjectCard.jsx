af001e084e40a2098a8c68a5d15e12373042c9ba
gaitano <dev.gaitano@gmail.com>
2026-05-08 10:40:27 +0300

    feat(structure): setup base ProjectList structure w/ mock data

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,13 @@
 import "./ProjectCard.css"
 
-function ProjectCard() {
+function ProjectCard({ thumbnail, name, description }) {
   return (
     <div className="wrapper-project-card">
-      Project Card component
+      <img src={thumbnail || "https://i.pinimg.com/736x/99/4e/63/994e6327635c4055c7caaf95810d50f3.jpg"} alt="project thumbnail" />
+      <div>
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   )
 }
--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -3,10 +3,38 @@ import "./ProjectList.css"
 import ProjectCard from "../ProjectCard/ProjectCard"
 
 function ProjectList() {
+  const projects = [
+    {
+      id: 1,
+      thumbnail: "https://res.cloudinary.com/diwkfbsgv/image/upload/v1775206748/logo_u4sz9t.svg",
+      name: "TopBox Studio",
+      description: " An AI powered content management system that helps teams create, organize, update, and publish digital content across multiple platforms. ",
+    },
+    {
+      id: 2,
+      thumbnail: "https://res.cloudinary.com/diwkfbsgv/image/upload/v1762926951/favicon_qzkvnv.png",
+      name: "Wayalink",
+      description: "A dual-interface logistics tracking system designed to improve end-to-end product visibility in fragmented supply chains. Built specifically for regions with low technology adoption ",
+    },
+    {
+      id: 3,
+      thumbnail: "https://res.cloudinary.com/diwkfbsgv/image/upload/v1777635327/nato-logo_rvlpns.png",
+      name: "Nato Fashion",
+      description: "A website revamp concept for Nato, an E-commerce website, offering a selection of high-end clothing and a premium shopping experience, focusing on elegance ",
+    },
+  ]
+
   return (
     <div className="wrapper-project-list">
-      Project List component
-      <ProjectCard />
+      <input type="search" name="search-projects" placeholder="Search projects" />
+      {projects.map((project) => (
+        <ProjectCard
+          key={project.id}
+          thumbnail={project.thumbnail}
+          name={project.name}
+          description={project.description}
+        />
+      ))}
     </div>
   )
 }
